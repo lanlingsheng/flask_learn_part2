8072503a6a08f81e5efb6da6ee402b02d5e3db68
lanlingsheng <gaoyan987@gmail.com>
2017-06-12 19:12:48 +0800

    添加 manage.py 启动脚本

--- config.py
+++ config.py
@@ -9,9 +9,14 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 class Config:
     SECRET_KEY = 'hard to guess string'
     SQLALCHEMY_COMMIT_ON_TEARDOWN = True
+    MAIL_SERVER = 'smtp.live.com'
+    MAIL_PORT = 587
+    MAIL_USER_TLS = True
+    MAIL_USERNAME = 'gaoyan0116@htmail.com'
+    MAIL_PASSWORD = 'gaoyan546=='
     FLASKY_MAIL_SUBJECT_PREFIX = '[Flasky]'
     FLASKY_MAIL_SENDER = 'Flasky Admin <>'
-    FLASKY_ADMIN = ''
+    FLASKY_ADMIN = '2467492608@qq.com'
 
     @staticmethod
     def init_app(app):
@@ -20,11 +25,6 @@ class Config:
 
 class DevelopmentConfig(Config):
     DEBUG = True
-    MAIL_SERVER = 'smtp.live.com'
-    MAIL_PORT = 587
-    MAIL_USER_TLS = True
-    MAIL_USERNAME = ''
-    MAIL_PASSWORD = ''
     SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'data-dev.sqlite')
 
 
