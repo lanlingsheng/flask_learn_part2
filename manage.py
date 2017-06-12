b971a6ea66b09d0d002324a3225b703aac55f371
lanlingsheng <gaoyan987@gmail.com>
2017-06-12 19:13:01 +0800

    添加 manage.py 启动脚本

--- manage.py
+++ manage.py
@@ -8,7 +8,7 @@ from flask_script import Manager, Shell
 from flask_migrate import Migrate, MigrateCommand
 
 
-app = create_app('default')
+app = create_app(os.getenv('FLASK_CONFIG') or 'default')
 manager = Manager(app)
 migrate = Migrate(app, db)
 
@@ -16,7 +16,14 @@ migrate = Migrate(app, db)
 def make_shell_context():
     return dict(app=app, db=db, User=User, Role=Role)
 manager.add_command("shell", Shell(make_context=make_shell_context))
-manager.add_command(db, MigrateCommand)
+manager.add_command('db', MigrateCommand)
+
+
+@manager.command
+def test():
+    import unittest
+    tests = unittest.TestLoader().discover('tests')
+    unittest.TextTestRunner(verbosity=2).run(tests)
 
 
 if __name__ == '__main__':
