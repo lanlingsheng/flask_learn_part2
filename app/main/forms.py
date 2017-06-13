fc24c5f6e424ade3cc3a404db4b75c6c9c898c10
lanlingsheng <gaoyan987@gmail.com>
2017-06-13 14:25:20 +0800

    Required 后面少了括号 Required()

--- app/main/forms.py
+++ app/main/forms.py
@@ -1,10 +1,10 @@
 # -*- coding: utf-8 -*-
 
-from flask_wtf import Form
+from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 from wtforms.validators import Required
 
 
-class NameForm(Form):
-    name = StringField('What is your name?', validators=[Required])
+class NameForm(FlaskForm):
+    name = StringField('What is your name?', validators=[Required()])
     submit = SubmitField('Submit')
